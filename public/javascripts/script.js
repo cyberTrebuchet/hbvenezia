7a233ce04c6ffccba2ccaf727c3423e25cb6bcac
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-15 14:30:44 -0400

    fun with images

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,9 @@
 $(function(){
   console.log('script.js linked!');
 
-  $('#doorway').click(function(e){
-    $('#doorway').fadeOut(3000);
-  })
+  $('#meet-me-img').click(function(e){
+    $('#meet-me-img').fadeOut(3000, function(){
+      $('#doorway-img').fadeIn(2000);
+    });
+  });
 });
