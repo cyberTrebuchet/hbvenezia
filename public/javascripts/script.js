fb8275f33647d9d0630788419dc78107523aa82e
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-19 16:28:28 -0400

    stripped of PureCSS and animations, per client request

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,15 +3,5 @@
 $(function(){
   console.log('script.js linked!');
 
-  $('#meet-me-img').hover(function(){
-    $('#meet-me-img').toggleClass('swing');
-  }, function(){
-    $('#meet-me-img').toggleClass('swing');
-  });
-
-  $('#meet-me-img').click(function(){
-    $('#meet-me-img').fadeOut(1000, function(){
-      $('#main').fadeIn(2000)
-    });
-  });
+  
 });
