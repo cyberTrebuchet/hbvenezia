44f3ae64e2a7520f5ec37109aa11fe93d11cbd93
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-20 17:03:44 -0400

    first link works with image in modal

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,5 +6,12 @@ $(function(){
   $('.tour').on('click', function(e){
     e.preventDefault();
     console.log(this.href);
+
+    $('#modal-img').attr('src', this.href);
+    $('#tour-modal').fadeIn(750);
+  });
+
+  $('#tour-modal').on('click', function(e){
+    $('#tour-modal').fadeOut(750);
   });
 });
