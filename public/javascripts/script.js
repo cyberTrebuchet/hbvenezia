e5f2b63b5ba08100d2f1e06061125a3998f09ce5
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-20 18:02:54 -0400

    link tour audio and images

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,10 +8,19 @@ $(function(){
     console.log(this.href);
 
     $('#modal-img').attr('src', this.href);
-    $('#tour-modal').fadeIn(750);
+    $('#tour-modal').fadeIn(1000);
   });
 
   $('#tour-modal').on('click', function(e){
     $('#tour-modal').fadeOut(750);
+  });
+
+  $('.audio').on('click', function(e){
+    e.preventDefault();
+    console.log(this.href);
+    $('audio')
+      .empty()
+      .append($('<source>').attr('src', this.href))
+      .trigger('play');
   });
 });
