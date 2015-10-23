dd02ea24f8c444a395c242396b9a0558561b5c72
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-23 17:07:21 -0400

    reflections view linked via routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router
+.get('/', function(req, res, next) {
   var titles = {
     title: 'Harry\'s Bar in Venice',
     desc: 'Cipriani Bar & Restaurant Venezia'
   }
   res.render('index', titles);
+})
+.get('/reflections', function(req, res, next) {
+  var titles = {
+    title: 'Harry\'s Bar in Venice',
+    desc: 'Cipriani Bar & Restaurant Venezia'
+  }
+  res.render('reflections', titles);
 });
 
 module.exports = router;
