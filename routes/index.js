2d5e1ba40e5362db02fc3f0cd9c8efaf5cd5e97b
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-27 16:56:16 -0400

    add privacy notice page

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,17 @@ router
 })
 .get('/reflections', function(req, res, next) {
   var titles = {
-    title: 'Harry\'s Bar in Venice',
+    title: 'Reflections on Harry\'s Bar',
     desc: 'Cipriani Bar & Restaurant Venezia'
   }
   res.render('reflections', titles);
+})
+.get('/privacy', function(req, res, next){
+  var titles = {
+    title: 'Privacy Policy',
+    desc: 'Harry\'s Bar Venezia unofficial site'
+  }
+  res.render('privacy', titles);
 });
 
 module.exports = router;
