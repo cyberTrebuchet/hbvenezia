e9ebe1ed0265ef82c551baa93bc46c26323866da
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-11-12 12:48:09 -0500

    realize that a new route is unnecessary if I just have everything in the /public directory

--- routes/index.js
+++ routes/index.js
@@ -26,25 +26,6 @@ router
     desc: 'Harry\'s Bar Venezia unofficial site'
   }
   res.render('privacy', titles);
-})
-.get('/sitemap.xml', function(req, res, next){
-  var options = {
-    root: __dirname + '/public/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
-    }
-  };
-  res.sendFile('/sitemap.xml', options, function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent:', '/public/sitemap.xml');
-    }
-  });
 });
 
 module.exports = router;
